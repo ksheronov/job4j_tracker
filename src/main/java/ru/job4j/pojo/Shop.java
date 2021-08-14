29884cdb8aebeebbb977c3029a0adda60a837c5f
ksheronov <skapank@mail.ru>
2021-08-14 17:53:08 +0300

    3.2. Удаление моделей из массива. [#310016]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -20,7 +20,7 @@ public class Shop {
         products[2] = new Product("Egg", 19);
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
-            /* проверяем, что объект не равен null. Т.к. у нас массив не заполнен целиком. */
+            /*  проверяем, что объект не равен null. Т.к. у нас массив не заполнен целиком.  */
             if (product != null) {
                 System.out.println(product.getName());
             }
