fd641dac48bf20bb58654ae29b53c731cd792140
ksheronov <skapank@mail.ru>
2021-10-21 11:45:58 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #218502]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -50,7 +50,7 @@ public class Tracker {
         boolean rsl = index != -1;
         if (rsl) {
             item.setId(id);
-            items[index] =  item;
+            items[index] = item;
         }
         return rsl;
     }
