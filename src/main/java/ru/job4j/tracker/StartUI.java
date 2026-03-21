98dfc4051e81d93d88514056692b37fbfc472176
ksheronov <skapank@mail.ru>
2026-03-21 14:00:42 +0300

    10. toString [#310018 #218531]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,8 +4,11 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item("Ivan");
+        Item item2 = new Item(100, "Sergey");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(formatter));
+        System.out.println(item);
+        System.out.println(item2);
     }
 }
