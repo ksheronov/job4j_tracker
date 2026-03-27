08486195c48296d68e04f0431d230d1e710d48a7
ksheronov <skapank@mail.ru>
2026-03-27 19:02:39 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element not found!!!");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] array = new String[]{"test", "test1", "test2"};
+        String key = "test22";
+        try {
+            int index = indexOf(array, key);
+            System.out.println("Index=" + index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
