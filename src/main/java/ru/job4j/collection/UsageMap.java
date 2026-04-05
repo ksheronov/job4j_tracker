3d1ca185b70393580808dcbb5e9270550b5635b5
ksheronov <skapank@mail.ru>
2026-04-05 19:27:38 +0300

    0. Map, HashMap [#212652 #561817]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("test@test", "Petr Ivanov");
         map.put("mail@mail", "Vera Petrova");
+        map.put("email@email", "Ivanov Ivan");
+        map.put("kris@mail", "Kristina Serova");
+        map.put("ks@mail.ru", "Konstantin Sh");
+        map.put("mail@mail", "Anna Kargina");
         for (String key : map.keySet()) {
             System.out.println("Key = " + key + " Value = " + map.get(key));
         }
