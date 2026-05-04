01f31047c037f59e8c6263916be7e6976220c3af
ksheronov <skapank@mail.ru>
2026-05-04 20:40:47 +0300

    14. Агрегатные методы min(), max(), count(), average(), sum() [#504928]

---
+++ src/main/java/ru/job4j/stream/MinExample.java
@@ -0,0 +1,34 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import ru.job4j.stream.mapto.Person;
+
+public class MinExample {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(4, 5, 1, 3, 2);
+        Optional<Integer> minEl = list.stream()
+                .min(Comparator.naturalOrder());
+        System.out.println(minEl.get());
+        OptionalInt min = IntStream
+                .rangeClosed(1, 5)
+                .min();
+        System.out.println(min.getAsInt());
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        Optional<Person> youngestPerson = people.stream()
+                .min(Comparator.comparing(Person::getAge));
+        int age = youngestPerson.get().getAge();
+        System.out.println(age);
+    }
+}
+
