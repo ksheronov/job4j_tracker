77bd632598345fe544c3d1e5fa5fcbb7b4bdf13e
ksheronov <skapank@mail.ru>
2026-05-04 21:54:37 +0300

    18. Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,92 @@
+package ru.job4j.stream;
+
+import java.time.LocalDate;
+
+public class Car {
+    private String brand;
+
+    private String model;
+
+    private LocalDate created;
+
+    private double volume;
+
+    private String color;
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private LocalDate created;
+        private double volume;
+        private String color;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildCreated(LocalDate created) {
+            this.created = created;
+            return this;
+        }
+
+        Builder buildVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.brand = brand;
+            car.model = model;
+            car.created = created;
+            car.volume = volume;
+            car.color = color;
+            return car;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Builder{"
+                + "brand='" + brand + '\''
+                + ", model='" + model + '\''
+                + ", created=" + created
+                + ", volume=" + volume
+                + ", color='" + color + '\''
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder()
+                .buildBrand("Toyota")
+                .buildModel("Camry")
+                .buildCreated(LocalDate.of(2021, 6, 1))
+                .buildColor("Red")
+                .buildVolume(2.5)
+                .build();
+        System.out.println(car);
+        Car car1 = new Builder()
+                .buildBrand("VW")
+                .buildModel("Tiguan")
+                .build();
+        System.out.println(car1);
+        Car car2 = new Builder()
+                .buildBrand("Volvo")
+                .buildModel("XC60")
+                .buildColor("Black")
+                .buildVolume(3.2)
+                .build();
+        System.out.println(car2);
+    }
+}
